2196b81a448b0cd2d8f5b7b21493ad1df04ace94
MikeBilbee <cmbilbee@gmail.com>
2024-07-11 19:05:04 -0500

    Mike 1.1 fixing linter errors

--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${host}:${port}`, {useNewUrlParser: true, useUnifiedTopology: true });
+    this.client = new MongoClient(`mongodb://${host}:${port}`, { useNewUrlParser: true, useUnifiedTopology: true });
     this.db = null;
     this.client.connect()
       .then(() => {
@@ -13,13 +13,16 @@ class DBClient {
       })
       .catch((err) => console.error('DB connection err', err));
   }
+
   isAlive() {
     return !!this.db;
   }
+
   async nbUsers() {
     if (!this.isAlive()) return 0;
     return this.db.collection('users').countDocuments();
   }
+
   async nbFiles() {
     if (!this.isAlive()) return 0;
     return this.db.collection('files').countDocuments();
@@ -27,4 +30,4 @@ class DBClient {
 }
 
 const dbClient = new DBClient();
-module.exports = DBClient;
+module.exports = dbClient;
