ecd5ecc3209a737dc3855bbd57565d5971f4a80d
MikeBilbee <cmbilbee@gmail.com>
2024-07-14 10:13:16 -0500

    Mike 3.0 we need to figure out these errors...

---
+++ controllers/UsersController.js
@@ -0,0 +1,42 @@
+// User controller
+
+const crypto = require('crypto');
+const dbClient = require('../utils/db');
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+
+    try {
+      const user = await dbClient.db().collection('users').findOne({ email });
+      if (user) {
+        return res.status(400).json({ error: 'Already exist' });
+      }
+
+      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+
+      const newUser = {
+        email,
+        password: hashedPassword,
+      };
+      const result = await dbClient.db().collection('users').insertOne(newUser);
+
+      return res.status(201).json({
+        id: result.insertedId,
+        email,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
+
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import AppController from '../controllers/AppController';
+const express = require('express');
+const UsersController = require('../controllers/UsersController');
+const AppController = require('../controllers/AppController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+router.post('/users', UsersController.postNew);
 
 export default router;
